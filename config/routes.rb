c69be58e02d7a0fe0339d96fe32905994a30c48d
Leandro Souza <leandroalvesdesouza@gmail.com>
2012-08-15 12:59:35 -0300

    Release

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TwitterForZombies::Application.routes.draw do
+  resources :zombies
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ db/migrate/20120810222230_remove_age_from_zombie.rb
@@ -0,0 +1,9 @@
+class RemoveAgeFromZombie < ActiveRecord::Migration
+  def up
+    remove_column :zombies, :age
+  end
+
+  def down
+    add_column :zombies, :age, :integer
+  end
+end
